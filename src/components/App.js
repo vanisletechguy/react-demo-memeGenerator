425990c6b8dc3c70dc27374a0a334ac21390a8ff
mikenic <temperanceaubie@gmail.com>
2018-07-04 23:10:15 -0700

    memes load with animations css

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import '../styles/index.css';
+import MemeItem from './MemeItem';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			memeLimit: 10 
+		}
+	}
 	render() {
 		return (
 			<div>
 				<h2>Welcome to the Meme Generator!</h2>
+				{
+					this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
+						return (
+							<MemeItem key={index} meme={meme}/>
+						)
+					})
+				}
+				<div className="meme-button" onClick={() => {
+					this.setState({memeLimit: this.state.memeLimit+10})
+				}}>Load 10 more memes... </div>
 			</div>
 
 		);
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return state;
+}
+
+export default connect(mapStateToProps, null)(App);
---
+++ src/components/MemeItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class MemeItem extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			hovered: false
+		}
+	}
+
+	render() {
+		return (
+			<div 
+				className="meme-item"
+				onMouseEnter={() => this.setState({ hovered:true })}
+				onMouseLeave={() => this.setState({ hovered:false })}
+			>
+				<img
+					src={this.props.meme.url}
+					alt={this.props.meme.name}
+					className={this.state.hovered ? "meme-img darken-img" : "meme-img"}/>
+				<p className={this.state.hovered ? "meme-txt" : "no-txt"}>
+					{this.props.meme.name}
+				</p>
+			</div>
+		)
+	}
+};
+
+export default MemeItem;
